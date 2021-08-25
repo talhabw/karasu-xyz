4874a6fa5814ddc74754fef3e7c33045d215a77d
Talha Karasu <talha@karasu.xyz>
2021-08-25 20:59:11 +0300

    add page for installl bunken extension

---
+++ src/pages/bunken.vue
@@ -0,0 +1,13 @@
+<template></template>
+
+<script>
+export default {
+  asyncData({ redirect }) {
+    redirect(
+      "https://addons.mozilla.org/firefox/downloads/file/3830505/bunken-1.0.1-an+fx.xpi"
+    )
+  },
+}
+</script>
+
+<style></style>
